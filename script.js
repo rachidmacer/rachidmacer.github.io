d15129b90befebaa2c3b293e29b34fbe0005ef84
“Rachid <macer_rach@bentley.edu>
2020-05-14 16:04:34 -0700

    update geo

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ var temp = document.querySelector('.temp');
 button.addEventListener('click',function(){
   fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=efe51b26290b5235f3b2ee0e134fbec9')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+      var nameValue = data['name'];
+      var tempValue = data['main']['temp'];
+      var descValue = data['weather'][0]['description'];
+
+      name.innerHTML = nameValue;
+      desc.innerHTML = descValue;
+      temp.innerHTML = tempValue;
+
+    })
 
   .catch(err => alert("Wrong City Name!""))
 })
