3ac55ea8e0b72c754fff06392e6a38f335aa75b0
“Rachid <macer_rach@bentley.edu>
2020-05-14 15:19:57 -0700

    geo

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-var lat = document.getElementById('latitude').textContent;
-var lon = document.getElementById('longitude').textContent;
+
 
 function getLocation() {
   if(navigator.getlocation){
     navigator.geolocation.getCurrentPosition( position => {
       const lat = position.coords.latitude;
       const lon = position.coors.longitude;
+      var lat = document.getElementById('latitude').textContent;
+      var lon = document.getElementById('longitude').textContent;
       console.log("position");
     });
   } else {
