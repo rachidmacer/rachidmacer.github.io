3ca6391ff60115347bb28b30b640ee86b767e13b
“Rachid <macer_rach@bentley.edu>
2020-05-14 17:52:35 -0700

    spotify player

--- script.js
+++ script.js
@@ -22,3 +22,32 @@ button.addEventListener('click',function(){
 
 
 //Opens window
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token = 'BQBteChwzh4gcTKVQePkUrS6x2MGNsCHOkOwDXohnnrmWzsTWFBfZP8LzdHNPXP96ch_xiw9Kx9s_qoCZS-SyaXqBFQIHR7vT-WNdJUPzEL2EU_07GqJ7UYixUgZFAxzNoYTpiGkDOkXQuIBKJ8884_1GcwwJwoZsGg';
+  const player = new Spotify.Player({
+    name: 'Web Playback SDK Quick Start Player',
+    getOAuthToken: cb => { cb(token); }
+  });
+
+  // Error handling
+  player.addListener('initialization_error', ({ message }) => { console.error(message); });
+  player.addListener('authentication_error', ({ message }) => { console.error(message); });
+  player.addListener('account_error', ({ message }) => { console.error(message); });
+  player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+  // Playback status updates
+  player.addListener('player_state_changed', state => { console.log(state); });
+
+  // Ready
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+  });
+
+  // Not Ready
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+  });
+
+  // Connect to the player!
+  player.connect();
+};
