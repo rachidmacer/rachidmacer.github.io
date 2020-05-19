878f4ed47dffdfa0219b3c00f3fe97e7fde5aad7
“Rachid <macer_rach@bentley.edu>
2020-05-19 12:25:24 -0700

    update to JS

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -1,28 +1,50 @@
 const api = {
-  key:"efe51b26290b5235f3b2ee0e134fbec9"
-  base: "https://api.openweathermap.org/data/2.5/"
+  key:"efe51b26290b5235f3b2ee0e134fbec9",
+  base:"https://api.openweathermap.org/data/2.5/"
 }
 
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress',setQuery);
 
 function setQuery(evt) {
-  if(evt.keyCode == 13) {
+  if (evt.keyCode == 13) {
     getResults(searchbox.value);
-    console.log(searchbox.value);
-  }
+   }
 }
 
 function getResults(query) {
-  fetch('${api.base}weather?=${query}&units=imperial&APPID=${api.key}')
+  fetch(`${api.base}weather?=${query}&units=imperial&APPID=${api.key}`)
   .then(weather => {
     return weather.json();
   }) .then(displayResults);
 }
 
 function displayResults(weather) {
-  console.log(weather);
   let city = document.querySelector('.location ,city');
-  city.innerText ='${weather.name}, ${weather.sys.country}'
+  city.innerText =`${weather.name}, ${weather.sys.country}`
 
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerText =`${Math.round(weather.main.temp)}<span>°F</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${math.round(weather.main.temp_min)}°F / ${math.round(weather.main.temp_max)}°F`;
+}
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
 }
