87fa882f3cb599e8ec4c650256ccfd5ee2087f63
“Rachid <macer_rach@bentley.edu>
2020-05-25 16:25:32 -0700

    weather app

--- WeatherApp/main.js
+++ WeatherApp/main.js
@@ -28,13 +28,13 @@ function displayResults (weather) {
   date.innerText = dateBuilder(now);
 
   let temp = document.querySelector('.current .temp');
-  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°F</span>`;
 
   let weather_el = document.querySelector('.current .weather');
   weather_el.innerText = weather.weather[0].main;
 
   let hilow = document.querySelector('.hi-low');
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°F / ${Math.round(weather.main.temp_max)}°F`;
 }
 
 function dateBuilder (d) {
