cabe8d3253f82121811970d2ef061a83c526b1fd
“Rachid <macer_rach@bentley.edu>
2020-05-27 23:20:18 -0700

    js

--- Stopwatch/main.js
+++ Stopwatch/main.js
@@ -1,6 +1,6 @@
 
-  var tens = 00;
   var seconds = 00;
+  var tens = 00;
   var appendTens = document.getElementById("tens")
   var appendSeconds = document.getElementById("seconds")
   var buttonStart = document.getElementById("button-start");
